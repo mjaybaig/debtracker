b0f235fea4ea4423babb83d00de586b1deec7b96
M. Jameel Baig <mjameelbaig@hotmail.com>
2017-01-10 17:20:45 +0500

    First commit and integrated openshift stuff

---
+++ server.js
@@ -0,0 +1,49 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+const server = http.createServer(app);
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+// const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// app.set('port', port);
+
+// /**
+//  * Create HTTP server.
+//  */
+// const server = http.createServer(app);
+
+// /**
+//  * Listen on provided port, on all network interfaces.
+//  */
+// server.listen(port, () => console.log(`API running on localhost:${port}`));
